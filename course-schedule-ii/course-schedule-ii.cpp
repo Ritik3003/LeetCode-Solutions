f1314a76e3f84a1464128114588486df945d9cc5
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-07-12 20:03:15 +0530

    Time: 26 ms (51.68%) | Memory: 13.2 MB (85.49%) - LeetSync

--- course-schedule-ii/course-schedule-ii.cpp
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>&v) {
-        int x=v.size();
+    vector<int> findOrder(int n, vector<vector<int>>& grid) {
         vector<int> adj[n];
-        for(int i=0; i<x; i++){
-            adj[v[i][1]].push_back(v[i][0]);
+        for(int i=0; i<grid.size(); i++){
+            adj[grid[i][1]].push_back(grid[i][0]);
         }
         vector<int> indegree(n,0);
-        queue<int> q;
         for(int i=0; i<n; i++){
             for(auto it: adj[i]){
                 indegree[it]++;
             }
         }
+        queue<int> q;
         for(int i=0; i<n; i++){
             if(indegree[i]==0){
                 q.push(i);
             }
         }
-        int count=0;
         vector<int> ans;
+        int count=0;
         while(q.size()){
             int node=q.front();
             q.pop();
-            count++;
             for(auto it: adj[node]){
                 indegree[it]--;
                 if(indegree[it]==0){
@@ -31,10 +29,10 @@ public:
                 }
             }
             ans.push_back(node);
+            count++;
         }
         if(count!=n){
-            vector<int> v;
-            return v;
+            return {};
         }
         return ans;
     }
