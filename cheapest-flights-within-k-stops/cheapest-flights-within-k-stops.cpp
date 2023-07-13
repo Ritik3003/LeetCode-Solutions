96ee8a805f50d3d3182872a23ec68fedf6ae054f
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-07-13 23:01:11 +0530

    Time: 20 ms (90.37%) | Memory: 14.3 MB (18.31%) - LeetSync

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -6,13 +6,15 @@ public:
             vector<int> x = it;
             adj[x[0]].push_back({x[1], x[2]});
         }
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        pq.push({0, {src, 0}});
+        // priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        set<pair<int,pair<int,int>>>pq;
+        pq.insert({0, {src, 0}});
         vector<int>dist(n,1e9);
         dist[src]=0;
         while(pq.size()){
-            auto it=pq.top();
-            pq.pop();
+            auto it=*pq.begin();
+            // pq.pop();
+            pq.erase(it);
             int stop=it.first;
             int node=it.second.first;
             int cost=it.second.second;
@@ -24,7 +26,7 @@ public:
                 int adjacent_node=x.first;
                 if( stop<=k && dist[adjacent_node]>cost+edge_weight){
                     dist[adjacent_node]=cost+edge_weight;
-                    pq.push({stop+1,{adjacent_node,cost+edge_weight}});
+                    pq.insert({stop+1,{adjacent_node,cost+edge_weight}});
                 }
             }
         }
