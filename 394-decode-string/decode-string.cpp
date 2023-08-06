fbf0afa1d0c8767fecbb1cd3f298a1f06820fe79
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-08-07 00:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (64.96%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+string solve(string &s, int &i){
+    string ans="";
+    while(i<s.size() && s[i]!=']'){
+        if(isdigit(s[i])){
+            int k=0;
+            while(i<s.size() && isdigit(s[i])){
+                k=k*10+(s[i++]-'0');
+            }
+            i++;
+            string result=solve(s,i);
+            while(k--){
+                ans+=result;
+            }
+            i++;
+        }else{
+            ans+=s[i];
+            i++;
+        }
+    }
+    return ans;
+}
+
+    string decodeString(string s) {
+        int i=0;
+        return solve(s,i);
+    }
+};
