eb45b66e7c14e2e6d72a312856a509d1953fe569
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-08-20 02:02:23 +0530

    Time: 119 ms (99.42%) | Memory: 73 MB (27.90%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    long long totalCost(vector<int>& costs, int k, int candidates) {
-        priority_queue<int,vector<int>, greater<int>> pq1,pq2;
+    long long totalCost(vector<int>& costs, int k, int candidates) { 
+        priority_queue<int,vector<int>,greater<int>> pq1,pq2;
         int n=costs.size();
         int i=0;
         int j=n-1;
@@ -13,8 +13,7 @@ public:
             while(pq2.size()<candidates && i<=j){
                 pq2.push(costs[j--]);
             }
-            int a=INT_MAX;
-            int b=INT_MAX;
+            int a=INT_MAX,b=INT_MAX;
             if(pq1.size()){
                 a=pq1.top();
             }
@@ -22,13 +21,13 @@ public:
                 b=pq2.top();
             }
             if(a<=b){
-                pq1.pop();
                 ans+=a;
+                pq1.pop();
             }else{
-                pq2.pop();
                 ans+=b;
+                pq2.pop();
             }
         }
         return ans;
-    }
+}
 };
