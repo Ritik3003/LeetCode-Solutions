d0e52ae8f03fa60233d7f40d651a48761534ac56
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-08-20 09:26:16 +0530

    Time: 123 ms (93.14%) | Memory: 106.1 MB (59.05%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.cpp
+++ 2725-mice-and-cheese/mice-and-cheese.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
         int n=reward1.size();
+        int ans=accumulate(reward2.begin(),reward2.end(),0);
         priority_queue<int> pq;
         for(int i=0; i<n; i++){
             pq.push(reward1[i]-reward2[i]);
         }
-        int ans=accumulate(reward2.begin(),reward2.end(),0);
         for(int i=0; i<k; i++){
             ans+=pq.top();
             pq.pop();
