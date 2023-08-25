e86827125dcb3f7cb44740c0632e7ca2cfb1b92d
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-08-25 19:33:22 +0530

    Time: 384 ms (68.61%) | Memory: 72.6 MB (52.33%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-int findparent(int node, vector<int>&parent){
+int find_parent(int node, vector<int>&parent){
     if(node==parent[node]){
         return node;
     }
-    return parent[node]=findparent(parent[node],parent);
+    return parent[node]=find_parent(parent[node],parent);
 }
+
 void union_set(int u, int v, vector<int>&parent, vector<int>&rank){
-    u=findparent(u,parent);
-    v=findparent(v,parent);
+    u=find_parent(u,parent);
+    v=find_parent(v,parent);
     if(rank[u]>rank[v]){
         rank[u]++;
         parent[v]=u;
@@ -18,30 +19,33 @@ void union_set(int u, int v, vector<int>&parent, vector<int>&rank){
     }
 }
 
-
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n=points.size();
-        vector<pair<int,pair<int,int>>> adj;
+        vector<pair<int,pair<int,int>>>v;
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 int distance=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-                adj.push_back({distance,{i,j}});
+                v.push_back({distance,{i,j}});
             }
         }
-        int ans=0;
-        sort(adj.begin(),adj.end());
-        int m=adj.size();
+        sort(v.begin(),v.end());
+        int m=v.size();
         vector<int> parent(m+1,0);
         vector<int> rank(m+1,0);
-        for(int i=1; i<=m; i++){
+        for(int i=0; i<=m; i++){
             parent[i]=i;
         }
+        int ans=0;
         for(int i=0; i<m; i++){
-            int u=findparent(adj[i].second.first,parent);
-            int v=findparent(adj[i].second.second,parent);
+            auto it=v[i];
+            int distance=it.first;
+            int x=it.second.first;
+            int y=it.second.second;
+            int u=find_parent(x,parent);
+            int v=find_parent(y,parent);
             if(u!=v){
-                ans+=adj[i].first;
-                union_set(adj[i].second.first,adj[i].second.second,parent,rank);
+                ans+=distance;
+                union_set(x,y,parent,rank);
             }
         }
         return ans;
