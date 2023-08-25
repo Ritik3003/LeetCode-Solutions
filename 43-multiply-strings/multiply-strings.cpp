3264e912e050c1c612f0ae8be79fd21071d2cd2a
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-08-25 12:25:07 +0530

    Time: 8 ms (55.25%) | Memory: 6.9 MB (30.86%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    string multiply(string num1, string num2) {
-        int n1=num1.size();
-        int n2=num2.size();
+    string multiply(string s1, string s2) {
+        int n1=s1.size();
+        int n2=s2.size();
         vector<int> nums(n1+n2,0);
         for(int i=n1-1; i>=0; i--){
             int k=i+n2;
             int carry=0;
-            if(num1[i]!='-'){
-                for(int j=n2-1; j>=0; j--){
-                    if(num2[j]!='-'){
-                        int x=((num1[i]-'0')*(num2[j]-'0')+carry)+nums[k];
-                        nums[k]=x%10;
-                        carry=x/10;
-                        k--;
-                    }
+            if(s1[i]!='-'){
+                for(int j=n2-1;j>=0; j--){
+                    int x=((s1[i]-'0')*(s2[j]-'0')+carry+nums[k]);
+                    carry=x/10;
+                    nums[k]=x%10;
+                    k--;
                 }
             }
             if(carry){
@@ -33,21 +31,10 @@ public:
                 break;
             }
         }
-        int count=0;
-        if(num1[0]=='-'){
-            count++;
-        }
-        if(num2[0]=='-'){
-            count++;
-        }
-        if(count%2){
-            ans+='-';
-        }
         reverse(ans.begin(),ans.end());
         if(ans==""){
             return "0";
         }
         return ans;
-
     }
 };
