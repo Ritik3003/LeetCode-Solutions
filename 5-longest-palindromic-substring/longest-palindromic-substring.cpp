99f2a7d724f0c86c867e8fabc3ee7268f234fd7c
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-10-27 18:35:15 +0530

    Time: 509 ms (24.07%) | Memory: 289.1 MB (13.15%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+
 bool check(string &s, int start, int end){
     while(start<=end){
         if(s[start]!=s[end]){
@@ -11,7 +12,7 @@ bool check(string &s, int start, int end){
     return true;
 }
 
-int solve(string &s, vector<vector<int>>&dp, int &maxi, int &index, int start, int end){
+int solve(string &s, vector<vector<int>>&dp, int start, int end, int &maxi, int &index){
     if(start>end){
         return 0;
     }
@@ -21,28 +22,33 @@ int solve(string &s, vector<vector<int>>&dp, int &maxi, int &index, int start, i
     if(dp[start][end]!=-1){
         return dp[start][end];
     }
-    if(check(s,start,end)){
-        int length=end-start+1;
-        if(length>maxi){
-            maxi=length;
-            index=start;
+    int a=0;
+    if(s[start]==s[end]){
+        if(check(s,start,end)){
+            int length=end-start+1;
+            if(length>maxi){
+                index=start;
+                maxi=length;
+            }
+            return dp[start][end]=length;
         }
-        return dp[start][end]=length;
     }
-    return dp[start][end]=max(solve(s,dp,maxi,index,start+1,end),solve(s,dp,maxi,index,start,end-1));
-
+    int b=solve(s,dp,start+1,end,maxi, index);
+    int c=solve(s,dp,start,end-1,maxi,index);
+    return dp[start][end]=max(b,c);
 }
 
     string longestPalindrome(string s) {
         int n=s.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        int ans=1;
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        int maxi=1;
+        // int ans=1;
+        string ans="";
         int index=0;
-        solve(s,dp,ans,index,0,n-1);
-        string x="";
-        while(ans--){
-            x+=s[index++];
+        solve(s,dp,0,n-1,maxi,index);
+        while(maxi--){
+            ans+=s[index++];
         }
-        return x;
+        return ans;
     }
 };
