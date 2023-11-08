0cb96a1f23ef58695578fb007e140a35ecc2bd77
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-11-09 01:51:22 +0530

    Time: 15 ms (61.02%) | Memory: 19.1 MB (21.27%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-int solve(vector<int>&nums, vector<vector<int>>&dp,int target, int i){
-    if(target<0){
+int solve(vector<int>&nums, vector<vector<int>>&dp, int target, int i){
+    if(i<0 || target<0){
         return 0;
-    }
-    if(i==0){
-        return target%nums[i]==0;
     }
     if(target==0 || (target==nums[i] && i==0)){
         return 1;
@@ -13,14 +10,17 @@ int solve(vector<int>&nums, vector<vector<int>>&dp,int target, int i){
     if(dp[i][target]!=-1){
         return dp[i][target];
     }
-    return dp[i][target]=solve(nums,dp,target,i-1) +solve(nums,dp,target-nums[i],i);
+    int not_take=solve(nums,dp,target,i-1);
+    int take=0;
+    if(target>=nums[i]){
+        take=solve(nums,dp,target-nums[i],i);
+    }
+    return dp[i][target]=take+not_take;
 }
 
-
-
-    int change(int target, vector<int>& coins) {
-        int n=coins.size();
+    int change(int target, vector<int>& nums) {
+        int n=nums.size();
         vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        return solve(coins,dp,target,n-1);
+        return solve(nums,dp,target,n-1);
     }
 };
