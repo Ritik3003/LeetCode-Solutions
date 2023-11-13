8ec465455bcba69e284f60f8fc32a1816f7712fb
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-11-13 09:44:44 +0530

    Time: 7 ms (17.27%) | Memory: 9.8 MB (90.78%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -24,17 +24,16 @@ int solve(vector<vector<int>>&matrix, int target){
         int n=matrix.size();
         int m=matrix[0].size();
         int index=solve(matrix,target);
-        // for(int i=0; i<n; i++){
-        //     if(matrix[i][0]==target){
-        //         return true;
-        //     }else if(matrix[i][0]<target){
-        //         index=i;
-        //     }
-        // }
-        cout<<index;
-        for(int i=0; i<m; i++){
-            if(matrix[index][i]==target){
+        int low=0;
+        int high=m-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(matrix[index][mid]==target){
                 return true;
+            }else if(matrix[index][mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
         return false;
