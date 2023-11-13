bb36e923c80d718860a056204cdacfd06c567817
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-11-13 09:43:02 +0530

    Time: 7 ms (17.27%) | Memory: 9.8 MB (90.78%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
+int solve(vector<vector<int>>&matrix, int target){
+    int n=matrix.size();
+    int m=matrix[0].size();
+    int low=0;
+    int ans=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(matrix[mid][0]==target){
+            return mid;
+        }else if(matrix[mid][0]>target){
+            high=mid-1;
+        }else{
+            ans=mid;
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
-        int index=0;
-        for(int i=0; i<n; i++){
-            if(matrix[i][0]==target){
-                return true;
-            }else if(matrix[i][0]<target){
-                index=i;
-            }
-        }
+        int index=solve(matrix,target);
+        // for(int i=0; i<n; i++){
+        //     if(matrix[i][0]==target){
+        //         return true;
+        //     }else if(matrix[i][0]<target){
+        //         index=i;
+        //     }
+        // }
+        cout<<index;
         for(int i=0; i<m; i++){
             if(matrix[index][i]==target){
                 return true;
