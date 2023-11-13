da9333f7cb742bb89bff31046d207de6bdb27963
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-11-13 11:54:40 +0530

    Time: 347 ms (19.78%) | Memory: 18.2 MB (12.71%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-int solve(vector<int>&nums, vector<int>&dp, int n, int i){
+
+int solve(vector<int>&nums, vector<int>&dp, int i, int n){
     if(i>=n){
         return 1e9;
     }
@@ -12,7 +13,7 @@ int solve(vector<int>&nums, vector<int>&dp, int n, int i){
     }
     int ans=1e9;
     for(int j=1; j<=nums[i]; j++){
-        ans=min(ans,solve(nums,dp,n,i+j)+1);
+        ans=min(ans,solve(nums,dp,j+i,n)+1);
     }
     return dp[i]=ans;
 }
@@ -20,6 +21,6 @@ int solve(vector<int>&nums, vector<int>&dp, int n, int i){
     int jump(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n,-1);
-        return solve(nums,dp,n,0);
+        return solve(nums,dp,0,n);
     }
 };
