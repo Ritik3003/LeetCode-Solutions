c7cc6372751318ca159744b6a6b214de6c274f74
Ritik Prajapati <79746424+Ritik3003@users.noreply.github.com>
2023-12-16 20:42:19 +0530

    Time: 4 ms (87.26%) | Memory: 7.7 MB (79.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        vector<int> s1(26,0),s2(26,0);
+        for(int i=0; i<s.size(); i++){
+            s1[s[i]-'a']++;
+        }
+        for(int i=0; i<t.size(); i++){
+            s2[t[i]-'a']++;
+        }
+        return s1==s2;
     }
 };
